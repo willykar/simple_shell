d3f1a3a625866ab127cc9fe756a45a36e4e3fe34
Symomkuu <simonnjunge350@gmail.com>
2023-11-15 16:44:18 -0800

    new

--- input_read.c
+++ input_read.c
@@ -66,31 +66,3 @@ char *error_path_p(data_s *datas)
 	free(ver_str);
 	return (error);
 }
-
-/**
- * rev_string - A function that reverses a string passed
- * @s: The input string
- */
-void rev_string(char *s)
-{
-	int count = 0, a, b;
-	char *str, temp;
-
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-	str = s;
-
-	for (a = 0; a < (count - 1); a++)
-	{
-		for (b = a + 1; b > 0; b--)
-		{
-			temp = *(str + b);
-			*(str + b) = *(str + (b - 1));
-			*(str + (b - 1)) = temp;
-		}
-	}
-}
